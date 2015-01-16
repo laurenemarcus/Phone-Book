d92e12c5072daf9296e5ccb47a90701e47580a21
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-16 09:31:00 -0800

    Return the phone number of the contact.

--- spec/phone_book_spec.rb
+++ spec/phone_book_spec.rb
@@ -9,4 +9,11 @@ describe(Contact) do
       expect(test_contact.name()).to(eq("Melody"))
     end
   end
+
+  describe("#name")do
+    it("returns the phone number of the contact") do
+      test_contact = Contact.new({ :name => "Melody", :phone_number => "555-444-1234" })
+      expect(test_contact.phone_number()).to(eq("555-444-1234"))
+    end
+  end
 end
